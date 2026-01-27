f300c152f793f3a25f89dc4a42d8885d118c0c2b
Vadim Godunko <vgodunko@gmail.com>
2026-01-28 00:40:59 +0400

    Target's opcodes.

--- source/iscsi-types.ads
+++ source/iscsi-types.ads
@@ -8,14 +8,25 @@ package iSCSI.Types with Pure is
 
    type Opcode_Type is private;
 
-   NOP_Out                               : constant Opcode_Type;
-   SCSI_Command                          : constant Opcode_Type;
-   SCSI_Task_Management_Function_Request : constant Opcode_Type;
-   Login_Request                         : constant Opcode_Type;
-   Text_Request                          : constant Opcode_Type;
-   SCSI_Data_Out                         : constant Opcode_Type;
-   Logout_Request                        : constant Opcode_Type;
-   SNACK_Request                         : constant Opcode_Type;
+   NOP_Out                                : constant Opcode_Type;
+   SCSI_Command                           : constant Opcode_Type;
+   SCSI_Task_Management_Function_Request  : constant Opcode_Type;
+   Login_Request                          : constant Opcode_Type;
+   Text_Request                           : constant Opcode_Type;
+   SCSI_Data_Out                          : constant Opcode_Type;
+   Logout_Request                         : constant Opcode_Type;
+   SNACK_Request                          : constant Opcode_Type;
+
+   NOP_In                                 : constant Opcode_Type;
+   SCSI_Response                          : constant Opcode_Type;
+   SCSI_Task_Management_Function_Response : constant Opcode_Type;
+   Login_Response                         : constant Opcode_Type;
+   Text_Response                          : constant Opcode_Type;
+   SCSI_Data_In                           : constant Opcode_Type;
+   Logout_Response                        : constant Opcode_Type;
+   Ready_To_Transfer                      : constant Opcode_Type;
+   Asynchronous_Message                   : constant Opcode_Type;
+   Reject                                 : constant Opcode_Type;
 
    type Stage is private;
 
@@ -25,16 +36,26 @@ package iSCSI.Types with Pure is
 
 private
 
-   type Opcode_Type is mod 2 ** 5 with Size => 5;
+   type Opcode_Type is mod 2 ** 6 with Size => 6;
 
-   NOP_Out                               : constant Opcode_Type := 16#00#;
-   SCSI_Command                          : constant Opcode_Type := 16#01#;
-   SCSI_Task_Management_Function_Request : constant Opcode_Type := 16#02#;
-   Login_Request                         : constant Opcode_Type := 16#03#;
-   Text_Request                          : constant Opcode_Type := 16#04#;
-   SCSI_Data_Out                         : constant Opcode_Type := 16#05#;
-   Logout_Request                        : constant Opcode_Type := 16#06#;
-   SNACK_Request                         : constant Opcode_Type := 16#10#;
+   NOP_Out                                : constant Opcode_Type := 16#00#;
+   SCSI_Command                           : constant Opcode_Type := 16#01#;
+   SCSI_Task_Management_Function_Request  : constant Opcode_Type := 16#02#;
+   Login_Request                          : constant Opcode_Type := 16#03#;
+   Text_Request                           : constant Opcode_Type := 16#04#;
+   SCSI_Data_Out                          : constant Opcode_Type := 16#05#;
+   Logout_Request                         : constant Opcode_Type := 16#06#;
+   SNACK_Request                          : constant Opcode_Type := 16#10#;
+   NOP_In                                 : constant Opcode_Type := 16#20#;
+   SCSI_Response                          : constant Opcode_Type := 16#21#;
+   SCSI_Task_Management_Function_Response : constant Opcode_Type := 16#22#;
+   Login_Response                         : constant Opcode_Type := 16#23#;
+   Text_Response                          : constant Opcode_Type := 16#24#;
+   SCSI_Data_In                           : constant Opcode_Type := 16#25#;
+   Logout_Response                        : constant Opcode_Type := 16#26#;
+   Ready_To_Transfer                      : constant Opcode_Type := 16#31#;
+   Asynchronous_Message                   : constant Opcode_Type := 16#32#;
+   Reject                                 : constant Opcode_Type := 16#3F#;
 
    type Stage is mod 2 ** 2 with Size => 2;
 
