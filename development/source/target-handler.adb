d2175c0abb4a5eef18c48c13f1fa061203558fe9
Vadim Godunko <vgodunko@gmail.com>
2026-02-15 21:02:22 +0400

    Decode WRITE(6)

--- development/source/target-handler.adb
+++ development/source/target-handler.adb
@@ -63,6 +63,11 @@ package body Target.Handler is
       Descriptor  : out SCSI.Commands.SPC.TEST_UNIT_READY_Command_Descriptor)
       return Boolean;
 
+   function Decode_WRITE_6
+     (CDB_Storage : A0B.Types.Arrays.Unsigned_8_Array;
+      Descriptor  : out SCSI.Commands.SBC.WRITE_Command_Descriptor)
+      return Boolean;
+
    procedure Failure_INVALID_COMMAND_OPERATION_CODE;
 
    function Failure_INVALID_FIELD_IN_CDB return Boolean;
@@ -84,6 +89,9 @@ package body Target.Handler is
 
    procedure Execute_TEST_UNIT_READY
      (Descriptor : SCSI.Commands.SPC.TEST_UNIT_READY_Command_Descriptor);
+
+   procedure Execute_WRITE
+     (Descriptor : SCSI.Commands.SBC.WRITE_Command_Descriptor);
 
    procedure Encode_READ_CAPACITY_Data
      (Allocation_Length : A0B.Types.Unsigned_32;
@@ -600,6 +608,58 @@ package body Target.Handler is
       return True;
    end Decode_TEST_UNIT_READY;
 
+   --------------------
+   -- Decode_WRITE_6 --
+   --------------------
+
+   function Decode_WRITE_6
+     (CDB_Storage : A0B.Types.Arrays.Unsigned_8_Array;
+      Descriptor  : out SCSI.Commands.SBC.WRITE_Command_Descriptor)
+      return Boolean
+   is
+      use type A0B.Types.Reserved_3;
+      use type A0B.Types.Unsigned_8;
+
+   begin
+      case Length_Check is
+         when Default | USB_MSC_BOOT =>
+            if CDB_Storage'Length /= SCSI.SBC4.CDB.WRITE_6_CDB_Length then
+               return Failure_INVALID_FIELD_IN_CDB;
+            end if;
+
+         when iSCSI =>
+            if CDB_Storage'Length /= iSCSI_CDB_Minumum_Length then
+               return Failure_INVALID_FIELD_IN_CDB;
+            end if;
+      end case;
+
+      declare
+         CDB : SCSI.SBC4.CDB.WRITE_6_CDB
+           with Import, Address => CDB_Storage'Address;
+
+      begin
+         if SCSI.SBC4.CDB.Reserved_1_7_5 (CDB) /= A0B.Types.Zero then
+            return Failure_INVALID_FIELD_IN_CDB;
+         end if;
+
+         --  XXX CONTROL is not validated/decoded
+
+         Descriptor :=
+           (GROUP_NUMBER          => 0,      --  Not present in WRITE(6)
+            LOGICAL_BLOCK_ADDRESS => SCSI.SBC4.CDB.LOGICAL_BLOCK_ADDRESS (CDB),
+            TRANSFER_LENGTH =>
+              (if CDB.TRANSFER_LENGTH = 0
+                 then 256
+                 else A0B.Types.Unsigned_32 (CDB.TRANSFER_LENGTH)),
+            WRPROTECT             => 0,      --  Not present in WRITE(6)
+            DPO                   => False,  --  Not present in WRITE(6)
+            FUA                   => False,  --  Not present in WRITE(6)
+            DLD                   => 0);     --  Not present in WRITE(6)
+      end;
+
+      return True;
+   end Decode_WRITE_6;
+
    ----------------------------
    -- Encode_MODE_SENSE_Data --
    ----------------------------
@@ -961,6 +1021,16 @@ package body Target.Handler is
          Read_Data_Length  => 0);
    end Execute_TEST_UNIT_READY;
 
+   -------------------
+   -- Execute_WRITE --
+   -------------------
+
+   procedure Execute_WRITE
+     (Descriptor : SCSI.Commands.SBC.WRITE_Command_Descriptor) is
+   begin
+      raise Program_Error;
+   end Execute_WRITE;
+
    --------------------------------------------
    -- Failure_INVALID_COMMAND_OPERATION_CODE --
    --------------------------------------------
@@ -1133,6 +1203,16 @@ package body Target.Handler is
                   else
                      raise Program_Error;
                      --  Command_Decode_Failure (Sense);
+                  end if;
+               end;
+
+            when SCSI.SBC4.WRITE_6 =>
+               declare
+                  Descriptor : SCSI.Commands.SBC.WRITE_Command_Descriptor;
+
+               begin
+                  if Decode_WRITE_6 (CDB_Storage, Descriptor) then
+                     Execute_WRITE (Descriptor);
                   end if;
                end;
 
